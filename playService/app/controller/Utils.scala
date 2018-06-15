f019936891a65eefd2c8ddc5ee6d4a23117bc161
Gregor Ihmor <gregor_alias1@outlook.de>
2018-06-15 18:43:20 +0200

    decode cookies

--- playService/app/controller/Utils.scala
+++ playService/app/controller/Utils.scala
@@ -3,10 +3,11 @@ package controller
 import java.util.UUID
 
 import play.api.mvc.Request
+import play.utils.UriEncoding
 
 object Utils {
   def playerName(request: Request[_]): Option[String] =
-    request.cookies.get("playerName").map(_.value)
+    request.cookies.get("playerName").map(_.value).map(UriEncoding.decodePath(_, "UTF-8"))
 
 
   def newShortId(): String = UUID.randomUUID().toString.take(8)
