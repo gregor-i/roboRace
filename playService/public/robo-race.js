acd56d9394547693bc0dee596c61df050f99a3b4
Gregor Ihmor <gregor_alias1@outlook.de>
2018-09-02 14:16:55 +0200

    simplyfied image store

--- playService/public/robo-race.js
+++ playService/public/robo-race.js
@@ -18068,60 +18068,46 @@ function frame(header, body, footer, modal) {
 module.exports = frame
 
 },{"snabbdom/h":3}],16:[function(require,module,exports){
-function image(url){
-    const img = new Image
-    img.src = url
-    return img
+function image(url) {
+  const img = new Image
+  img.src = url
+  return img
 }
 
 const iconClose = image('/assets/ic-close.svg')
 const iconGamerlist = image('/assets/ic-gamerlist.svg')
 const iconReplayAnimation = image('/assets/ic-replay-animation.svg')
 
-const player1 = image('/assets/player1.svg')
-const player2 = image('/assets/player2.svg')
-const player3 = image('/assets/player3.svg')
-const player4 = image('/assets/player4.svg')
-const player5 = image('/assets/player5.svg')
-const player6 = image('/assets/player6.svg')
+const players = [
+  image('/assets/player1.svg'),
+  image('/assets/player2.svg'),
+  image('/assets/player3.svg'),
+  image('/assets/player4.svg'),
+  image('/assets/player5.svg'),
+  image('/assets/player6.svg')
+]
+
+function player(index) {
+  return players[index % 6]
+}
 
 // https://materialdesignicons.com/icon/flag-variant-outline
 // const target = image('/assets/target.svg')
 
-
-function player(index){
-  switch (index % 6) {
-    case 0: return player1
-    case 1: return player2
-    case 2: return player3
-    case 3: return player4
-    case 4: return player5
-    case 5: return player6
-  }
+const actions = {
+  MoveForward: image('/assets/action-move-forward.svg'),
+  MoveBackward: image('/assets/action-move-backward.svg'),
+// StepRight : image('/assets/action_StepRight.svg'),
+// StepLeft : image('/assets/action_StepLeft.svg'),
+  MoveTwiceForward: image('/assets/action-move-forward-twice.svg'),
+  TurnRight: image('/assets/action-turn-right-60.svg'),
+  TurnLeft: image('/assets/action-turn-left-60.svg'),
+  UTurn: image('/assets/action-turn-left-180.svg'),
+  Sleep: image('/assets/action-sleep.svg'),
 }
 
-const MoveForward = image('/assets/action-move-forward.svg')
-const MoveBackward = image('/assets/action-move-backward.svg')
-// const StepRight = image('/assets/action_StepRight.svg')
-// const StepLeft = image('/assets/action_StepLeft.svg')
-const MoveTwiceForward = image('/assets/action-move-forward-twice.svg')
-const TurnRight = image('/assets/action-turn-right-60.svg')
-const TurnLeft = image('/assets/action-turn-left-60.svg')
-const UTurn = image('/assets/action-turn-left-180.svg')
-const Sleep = image('/assets/action-sleep.svg')
-
 function action(name) {
-  switch (name){
-    case 'MoveForward':      return MoveForward
-    case 'MoveBackward':     return MoveBackward
-    // case 'StepRight':        return StepRight
-    // case 'StepLeft':         return StepLeft
-    case 'MoveTwiceForward': return MoveTwiceForward
-    case 'TurnRight':        return TurnRight
-    case 'TurnLeft':         return TurnLeft
-    case 'UTurn':            return UTurn
-    case 'Sleep':            return Sleep
-  }
+  return actions[name]
 }
 
 module.exports = {
